e3bdaacc6e2e795208b7c28a5d63f94330ccc02b
Fedchenko Artem <artem_fedchenko_2017@mail.ru>
2025-05-15 23:25:21 +0700

    delete: неиспользуемый компонент

--- src/components/customInput/types.ts
+++
@@ -1,9 +0,0 @@
-export interface ICustomInput {
-  maxHeight: number;
-  defaultValue?: string;
-  fontWeight?: number;
-  onChange?: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
-  handleError?: (value: string) => string | null;
-  hasSubmitted?: boolean;
-  setIsEditing?: React.Dispatch<React.SetStateAction<boolean>>;
-}
